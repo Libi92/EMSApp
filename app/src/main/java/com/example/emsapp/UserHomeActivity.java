d283a92de9e0502665bf092a14ab0b7b25993019
libi92 <*Foss2k12>
2021-06-02 22:55:10 +0530

    nav header update

--- app/src/main/java/com/example/emsapp/UserHomeActivity.java
+++ app/src/main/java/com/example/emsapp/UserHomeActivity.java
@@ -2,6 +2,9 @@ package com.example.emsapp;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,6 +14,9 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.emsapp.util.Globals;
 import com.google.android.material.navigation.NavigationView;
 
 public class UserHomeActivity extends AppCompatActivity {
@@ -35,6 +41,26 @@ public class UserHomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        updateNavHeader(navigationView);
+    }
+
+    private void updateNavHeader(NavigationView navigationView) {
+        View headerView = navigationView.getHeaderView(0);
+        if (headerView != null) {
+            ImageView imageViewNavUser = headerView.findViewById(R.id.imageViewNavUser);
+            TextView navUsername = headerView.findViewById(R.id.textViewNavUserName);
+            TextView navEmail = headerView.findViewById(R.id.textViewNavEmail);
+
+            navUsername.setText(Globals.user.getDisplayName());
+            navEmail.setText(Globals.user.getEmail());
+            RequestOptions requestOptions = new RequestOptions()
+                    .placeholder(R.mipmap.ic_launcher);
+            Glide.with(this)
+                    .setDefaultRequestOptions(requestOptions)
+                    .load(Globals.user.getPhoto())
+                    .into(imageViewNavUser);
+        }
     }
 
     @Override
